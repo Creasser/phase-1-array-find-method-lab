f8af444ad391811bfd1e4976788a41ee1719ca6e
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-06 21:00:53 -0600

    commit tree de8259134cccb01ae14f776dc41c48d2806836e3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arr){
+    let ans = arr.find(item => item.result === 'W')
+        if (ans){
+            return ans.year
+            
+        }else{
+            return undefined
+        }
+    }
+
+
